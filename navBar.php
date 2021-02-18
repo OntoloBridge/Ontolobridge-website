16ee789ca12fc5014930f57f6cb33eda7e5a4d1d
Whinis <whinis@whinis.com>
2021-02-18 10:35:29 -0500

    Added password reset pages

--- navBar.php
+++ navBar.php
@@ -1,7 +1,7 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
     <h5 class="my-0 mr-md-auto font-weight-normal"><a href="/" class="text-decoration-none text-dark" >Ontolobridge</a></h5>
     <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark" href="/api/">API</a>
+        <a class="p-2 text-dark" href="http://localhost:8080">API</a>
         <a class="p-2 text-dark" href="about.php">About</a>
         <a class="p-2 text-dark" href="#">Help</a>
     </nav>
--- reset_password.php
+++ reset_password.php
@@ -28,15 +28,53 @@ if(isset($_POST['email'])) {
     }
     $_SESSION['message'] = "If a user is associated with this email a reset should have been sent";
 }
+if(isset($_POST['token'])) {
+
+    $curl->setHeader('Accept', 'application/json');
+    $postData =[];
+
+    // assign username and password to psot data
+    $postData['token'] = $_POST['token'];
+    $postData['password'] = $_POST['password'];
+
+    //post to login requests
+    $response = $curl->get(Constants::ONTOLOBRIDGE_URL."user/reset_password",$postData);
+    $httpCode = $curl->http_status_code;
+    $data = json_decode($response, true);
+
+    //if we had a code of 0 something went wrong or timed out
+    if ($httpCode === 0) {
+        $_SESSION['message'] = "backend has went away";
+        redirect("/reset_password");
+    }
+    //code not 200 means we should have an error back
+    //TODO: Check if Missing user role and redirect to complete registration
+    if ($httpCode !== 200) {
+        $_SESSION['message'] = $data['message'];
+        redirect("/reset_password");
+    }
+    $_SESSION['message'] = "If a user is associated with this email a reset should have been sent";
+}
 ?>
 <?php include("header.php"); ?>
 <script type="text/javascript">
     $(function() {
         $(document).on('click', '#btnSend', function () {
-            $("<input />").attr("type", "hidden")
-                .attr("name", "email")
-                .attr("value", $("#email").val())
-                .appendTo("#form");
+            <?php if(!isset($_GET['token'])) { ?>
+                $("<input />").attr("type", "hidden")
+                    .attr("name", "email")
+                    .attr("value", $("#email").val())
+                    .appendTo("#form");
+            <?php }else{ ?>
+                $("<input />").attr("type", "hidden")
+                    .attr("name", "token")
+                    .attr("value", "<?php echo $_GET['token'] ?>")
+                    .appendTo("#form");
+                $("<input />").attr("type", "hidden")
+                    .attr("name", "password")
+                    .attr("value", $("#password1").val())
+                    .appendTo("#form");
+            <?php } ?>
             $("#form").trigger("submit");
         });
         $(document).on('keydown',function(e) {
@@ -54,25 +92,53 @@ if(isset($_POST['email'])) {
     <div class="container d-flex h-100">
         <div class="row align-self-center w-100">
             <div class="col-6 mx-auto">
-                <div class="jumbotron">
-                    <div class="row text-center">
-                        <div class="col text-center font-weight-bold">
-                            Reset Password
+                <?php if(!isset($_GET['token'])) { ?>
+                    <div class="jumbotron">
+                        <div class="row text-center">
+                            <div class="col text-center font-weight-bold">
+                                Reset Password
+                            </div>
+                        </div>
+                        <?php include("util/displayMessage.php"); ?>
+                        <div class="row">
+                            <div class="col">Email:</div>
+                        </div>
+                        <div class="row">
+                            <div class="col"><input class ="container-fluid" type="text" id="email"></div>
+                        </div>
+                        <hr>
+                        <div class="row text-center">
+                            <form action="/reset_password" method="post" id="form" name="form"></form>
+                            <div class="col"><div  class="btn btn-primary btn-lg" id="btnSend">Request Reset</div></div>
                         </div>
                     </div>
-                    <?php include("util/displayMessage.php"); ?>
-                    <div class="row">
-                        <div class="col">Email:</div>
+                <?php }else{ ?>
+                    <div class="jumbotron">
+                        <div class="row text-center">
+                            <div class="col text-center font-weight-bold">
+                                Reset Password
+                            </div>
+                        </div>
+                        <?php include("util/displayMessage.php"); ?>
+                        <div class="row">
+                            <div class="col">Password:</div>
+                        </div>
+                        <div class="row">
+                            <div class="col"><input class ="container-fluid" type="text" id="password1"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col">Password Confirm:</div>
+                        </div>
+                        <div class="row">
+                            <div class="col"><input class ="container-fluid" type="text" id="password2"></div>
+                        </div>
+                        <hr>
+                        <div class="row text-center">
+                            <form action="/reset_password" method="post" id="form" name="form"></form>
+                            <div class="col"><div  class="btn btn-primary btn-lg" id="btnSend">Change Password</div></div>
+                        </div>
                     </div>
-                    <div class="row">
-                        <div class="col"><input class ="container-fluid" type="text" id="email"></div>
-                    </div>
-                    <hr>
-                    <div class="row text-center">
-                        <form action="/reset_password" method="post" id="form" name="form"></form>
-                        <div class="col"><div  class="btn btn-primary btn-lg" id="btnSend">Request Reset</div></div>
-                    </div>
-                </div>
+                <?php } ?>
             </div>
         </div>
     </div>
